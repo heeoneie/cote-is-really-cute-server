9bc6361104d9e40338043726ce7ce38d16b487a1
heeeione <heewon410@naver.com>
2024-10-02 11:49:25 +0900

    chore: Init migration from ExpressJS to Spring Boot

---
+++ src/main/java/com/example/coteisreallycute/CoteIsReallyCuteApplication.java
@@ -0,0 +1,13 @@
+package com.example.coteisreallycute;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CoteIsReallyCuteApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CoteIsReallyCuteApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/coteisreallycute/CoteIsReallyCuteApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.coteisreallycute;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CoteIsReallyCuteApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
