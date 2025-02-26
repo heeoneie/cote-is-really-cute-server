a8cfd2df1cdc6a04e209a0157f5d2bb3a05cf979
heeeione <heewon410@naver.com>
2025-02-26 18:20:44 +0900

    refactor: Migration authMiddleware to TS

--- src/middlewares/authMiddleware.ts
+++ src/middlewares/authMiddleware.ts
@@ -1,23 +1,35 @@
-import jwt from 'jsonwebtoken';
+import { Request, Response, NextFunction } from 'express';
+import jwt, { JwtPayload } from 'jsonwebtoken';
 
-export const authMiddleware = (req, res, next) => {
+interface RequestWithUser extends Request {
+  user?: JwtPayload | string;
+}
+
+export const authMiddleware = (
+  req: RequestWithUser,
+  res: Response,
+  next: NextFunction,
+): void => {
   const authHeader = req.header('Authorization');
-  if (!authHeader)
-    return res.status(401).json({ msg: 'Authorization header is missing' });
+  if (!authHeader) {
+    res.status(401).json({ msg: 'Authorization header is missing' });
+    return;
+  }
 
   const token = authHeader.startsWith('Bearer ')
     ? authHeader.replace('Bearer ', '')
     : authHeader;
 
-  if (!token)
-    return res
-      .status(401)
-      .json({ msg: 'No token provided, authorization denied' });
+  if (!token) {
+    res.status(401).json({ msg: 'No token provided, authorization denied' });
+    return;
+  }
 
   try {
-    req.user = jwt.verify(token, process.env.JWT_SECRET);
+    req.user = jwt.verify(token, process.env.JWT_SECRET as string);
     next();
   } catch (err) {
-    return res.status(401).json({ msg: 'Token is not valid or has expired' });
+    res.status(401).json({ msg: 'Token is not valid or has expired' });
+    return;
   }
 };
